6bb1d6461793196f727ce43a00921ac8a2b6d113
Christoph Koehler <christoph@webempoweredchurch.org>
2007-01-24 03:02:11 +0000

    Fix #10, also added Primary Key to the table

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,8 +3,10 @@
 --
 
 CREATE TABLE `tx_wecmap_cache` (
-  address_hash varchar(50) NOT NULL default '',
-  address varchar(100) NOT NULL default '',
-  latitude float default 0,
-  longitude float default 0,
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  	address_hash varchar(50) NOT NULL default '',
+  	address varchar(100) NOT NULL default '',
+  	latitude float default '0',
+  	longitude float default '0',
+	
+	PRIMARY KEY (address_hash)
+);
