3032ad31affebbbe82324ab86f48dde06aada224
Jeff Segars <jeff@webempoweredchurch.org>
2007-02-13 01:26:11 +0000

    Added XCLASS definitions.

--- class.tx_wecmap_batchgeocode.php
+++ class.tx_wecmap_batchgeocode.php
@@ -207,4 +207,8 @@ class tx_wecmap_batchgeocode {
 	}
 }
 
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/wec_map/class.tx_wecmap_batchgeocode.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/wec_map/class.tx_wecmap_batchgeocode.php']);
+}
+
 ?>
