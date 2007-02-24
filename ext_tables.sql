d1e0daa24099298e54fd0f4b0859ae60b92b30e1
Christoph Koehler <christoph@webempoweredchurch.org>
2007-02-24 07:23:26 +0000

    merge [217] and [218] back into trunk

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,8 +5,8 @@
 CREATE TABLE `tx_wecmap_cache` (
   	address_hash varchar(50) NOT NULL default '',
   	address varchar(100) NOT NULL default '',
-  	latitude float default '0',
-  	longitude float default '0',
+  	latitude double default '0',
+  	longitude double default '0',
 	
 	PRIMARY KEY (address_hash)
 );
