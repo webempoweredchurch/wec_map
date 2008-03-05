8153c57bf2655e472ab0a38044bdb042277dc225
Christoph Koehler <christoph@webempoweredchurch.org>
2008-03-05 10:14:32 +0000

    wrong place for this class

--- class.tx_wecmap_markergroup.php
+++ class.tx_wecmap_markergroup.php
@@ -39,6 +39,88 @@
  * @subpackage tx_wecmap
  */
 class tx_wecmap_markergroup {
+	var $markers;		// array of marker objects
+	var $markerCount; 	// convenience variable with number of markers = sizeof($markers);
+	var $id;			// unique identifier of this group
+	var $mapName;		// the name of the map this group belongs to
+	var $minZoom;		// min zoom level for this group
+	var $maxZoom;		// max zoom level for this group
+	
+	/**
+	 * PHP4 Constructor
+	 * @param int unique id of this group
+	 * 
+	 * @return void
+	 **/
+	function tx_wecmap_markergroup($id, $minZoom, $maxZoom) {
+		$this->__construct($id, $minZoom, $maxZoom);
+	}
+	
+	/**
+	 * PHP5 constructor
+	 *
+	 * @return void
+	 **/
+	function __construct($id, $minZoom, $maxZoom) {
+		$this->id = $id;
+		$this->minZoom = $minZoom;
+		$this->maxZoom = $maxZoom;
+	}
+	
+	/**
+	 * returns the js array
+	 *
+	 * @return void
+	 **/
+	function drawMarkerJS() {
+		
+	}
+	
+	/**
+	 * adds a marker object to this group
+	 *
+	 * @return void
+	 **/
+	function addMarker($markerObj) {
+		$this->markers[] = $markerObj;
+		$this->markerCount++;
+	}
+	
+	/**
+	 * return min zoom level
+	 *
+	 * @return int
+	 **/
+	function getMinZoom() {
+		return $this->minZoom;
+	}
+	
+	/**
+	 * return max zoom
+	 *
+	 * @return int
+	 **/
+	function getMaxZoom() {
+		return $this->maxZoom;
+	}
+	
+	/**
+	 * return the number of markers in this group
+	 *
+	 * @return int
+	 **/
+	function getMarkerCount() {
+		return $this->markerCount;
+	}
+	
+	/**
+	 * set map name
+	 *
+	 * @return void
+	 **/
+	function setMapName($name) {
+		$this->mapName = $name;
+	}
 	
 }
 
